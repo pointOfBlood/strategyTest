3294eda2f750282e8e342bb49ebd7a91dc0fc4ef
asdfghjkl123 <504794537@qq.com>
2017-02-17 09:58:15 +0800

    add base file

---
+++ strategy.py
@@ -0,0 +1,34 @@
+# 初始化函数，设定要操作的股票、基准等等
+def initialize(context):
+    # 定义一个全局变量, 保存要操作的股票
+    # 000001(股票:平安银行)
+    g.security = '000001.XSHE'
+    #设定沪深300作为基准
+    set_benchmark('000300.XSHG')
+
+# 每个单位时间(如果按天回测,则每天调用一次,如果按分钟,则每分钟调用一次)调用一次
+def handle_data(context, data):
+    security = g.security
+    # 获取股票的收盘价
+    close_data = attribute_history(security, 5, '1d', ['close'])
+    # 取得过去五天的平均价格
+    MA5 = close_data['close'].mean()
+    # 取得上一时间点价格
+    current_price = close_data['close'][-1]
+    # 取得当前的现金
+    cash = context.portfolio.cash
+
+    # 如果上一时间点价格高出五天平均价1%, 则全仓买入
+    if current_price > 1.01*MA5:
+        # 用所有 cash 买入股票
+        order_value(security, cash)
+        # 记录这次买入
+        log.info("Buying %s" % (security))
+    # 如果上一时间点价格低于五天平均价, 则空仓卖出
+    elif current_price < MA5 and context.portfolio.positions[security].closeable_amount > 0:
+        # 卖出所有股票,使这只股票的最终持有量为0
+        order_target(security, 0)
+        # 记录这次卖出
+        log.info("Selling %s" % (security))
+    # 画出上一时间点价格
+    record(stock_price=current_price)
